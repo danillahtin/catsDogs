c06caca99c495a9bfeec6ce30eb8bde14ca99f48
Danil Lakhtin <danil_lahtin@mail.ru>
2020-05-30 17:55:15 +0300

    [+] subscribe after successful load notifies with previously loaded cats

--- CoreTests/CatServiceTests.swift
+++ CoreTests/CatServiceTests.swift
@@ -42,13 +42,21 @@ final class CatsLoader {
 
 final class CatService {
     let loader: CatsLoader
+    private var cats: [Cat]?
     
     init(loader: CatsLoader) {
         self.loader = loader
     }
     
     func subscribe(onNext: @escaping ([Cat]) -> ()) {
-        loader.load(onNext)
+        if let cats = cats {
+            return onNext(cats)
+        }
+        
+        loader.load { [weak self] in
+            self?.cats = $0
+            onNext($0)
+        }
     }
 }
 
@@ -74,6 +82,21 @@ class CatServiceTests: XCTestCase {
         
         let cats = [Cat(), Cat(), Cat()]
         loader.complete(with: cats)
+        
+        XCTAssertEqual(retrievedCats, [cats])
+    }
+    
+    func test_subscribeAfterSuccessfulLoad_notifiesWithPreviouslyLoadedCats() {
+        let (sut, loader) = makeSut()
+        
+        sut.subscribe(onNext: { _ in })
+        let cats = [Cat(), Cat(), Cat()]
+        loader.complete(with: cats)
+        
+        var retrievedCats: [[Cat]] = []
+        sut.subscribe(onNext: {
+            retrievedCats.append($0)
+        })
         
         XCTAssertEqual(retrievedCats, [cats])
     }
