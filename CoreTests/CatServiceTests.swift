c82580d7656803250ac83d949ae6f43c0c784859
Danil Lakhtin <danil_lahtin@mail.ru>
2020-05-30 17:40:26 +0300

    [+] subscribe loads cats

--- CoreTests/CatServiceTests.swift
+++ CoreTests/CatServiceTests.swift
@@ -9,25 +9,48 @@
 import XCTest
 
 
-final class CatService {
+final class CatsLoader {
+    var loadCallCount = 0
     
+    func load() {
+        loadCallCount += 1
+    }
+}
+
+final class CatService {
+    let loader: CatsLoader
+    
+    init(loader: CatsLoader) {
+        self.loader = loader
+    }
+    
+    func subscribe() {
+        loader.load()
+    }
 }
 
 class CatServiceTests: XCTestCase {
-    func test_init() {
-        let sut = makeSut()
+    func test_subscribe_loadsCats() {
+        let (sut, loader) = makeSut()
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+        sut.subscribe()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
     
     // MARK: - Helpers
     
     private func makeSut(
         file: StaticString = #file,
-        line: UInt = #line) -> CatService
+        line: UInt = #line) -> (sut: CatService, loader: CatsLoader)
     {
-        let sut = CatService()
+        let loader = CatsLoader()
+        let sut = CatService(loader: loader)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: sut, file: file, line: line)
         
-        return sut
+        return (sut, loader)
     }
 }
