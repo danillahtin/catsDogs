7c7f96a3e60aee51f2837382d79cade07e926985
Danil Lakhtin <danil_lahtin@mail.ru>
2020-05-31 16:39:43 +0300

    [+] loadView subscribes

--- UITests/CatListViewControllerTests.swift
+++ UITests/CatListViewControllerTests.swift
@@ -8,14 +8,26 @@
 
 import XCTest
 
+protocol Publisher {
+    func subscribe()
+}
 
 final class CatListViewController: UIViewController {
     let tableView = UITableView()
+    
+    private var publisher: Publisher!
+    
+    convenience init(publisher: Publisher) {
+        self.init()
+        
+        self.publisher = publisher
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
+        publisher.subscribe()
     }
 }
 
@@ -31,21 +43,42 @@ extension CatListViewController: UITableViewDataSource {
 
 class CatListViewControllerTests: XCTestCase {
     func test_loadView_rendersEmptyList() {
-        XCTAssertEqual(makeSut().renderedViewsCount, 0)
+        let sut = makeSut()
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.renderedViewsCount, 0)
+    }
+    
+    func test_loadView_subscribes() {
+        let publisher = PublisherStub()
+        let sut = makeSut(publisher: publisher)
+        
+        XCTAssertEqual(publisher.subscribers.count, 0)
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(publisher.subscribers.count, 1)
     }
     
     // MARK: - Helpers
     
     private func makeSut(
+        publisher: PublisherStub = .init(),
         file: StaticString = #file,
         line: UInt = #line) -> CatListViewController
     {
-        let sut = CatListViewController()
-        sut.loadViewIfNeeded()
+        let sut = CatListViewController(publisher: publisher)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: publisher, file: file, line: line)
         
         return sut
+    }
+    
+    private final class PublisherStub: Publisher {
+        var subscribers: [Void] = []
+        
+        func subscribe() {
+            subscribers.append(())
+        }
     }
 }
 
