0ca00f08c09c3639900a2b3ea058d07f913cdc12
Danil Lakhtin <danil_lahtin@mail.ru>
2020-05-31 11:31:38 +0300

    [*] Removes LoadingStorageTests dependency on Cat type. Replaces it with simple UUID.

--- CoreTests/LoadingStorageTests.swift
+++ CoreTests/LoadingStorageTests.swift
@@ -9,56 +9,57 @@
 import XCTest
 import Core
 
-private typealias Service = LoadingStorage<LoadingStorageTests.CatsLoaderSpy>
+private typealias Entity = UUID
+private typealias Service = LoadingStorage<LoadingStorageTests.EntitiesLoaderSpy>
 
 class LoadingStorageTests: XCTestCase {
     
-    func test_subscribe_loadsCats() {
+    func test_subscribe_loadsEntities() {
         let (sut, loader) = makeSut()
         
         XCTAssertEqual(loader.loadCallCount, 0)
-        sut.subscribeCats()
+        sut.subscribeEntities()
         
         XCTAssertEqual(loader.loadCallCount, 1)
-        sut.subscribeCats()
+        sut.subscribeEntities()
         
         XCTAssertEqual(loader.loadCallCount, 2)
         loader.complete(with: [], at: 1)
-        sut.subscribeCats()
+        sut.subscribeEntities()
         
         XCTAssertEqual(loader.loadCallCount, 2)
     }
     
-    func test_loadCompletionWithCats_notifies() {
+    func test_loadCompletionWithEntities_notifies() {
         let (sut, loader) = makeSut()
         
-        let observer0 = SpyObserver<[Cat]>(sut: sut)
-        let observer1 = SpyObserver<[Cat]>(sut: sut)
+        let observer0 = SpyObserver<[Entity]>(sut: sut)
+        let observer1 = SpyObserver<[Entity]>(sut: sut)
         
         XCTAssertEqual(observer0.retrieved, [])
         XCTAssertEqual(observer1.retrieved, [])
         
-        let cats = makeCats()
-        loader.complete(with: cats)
+        let entities = makeEntities()
+        loader.complete(with: entities)
         
-        XCTAssertEqual(observer0.retrieved, [cats])
-        XCTAssertEqual(observer1.retrieved, [cats])
+        XCTAssertEqual(observer0.retrieved, [entities])
+        XCTAssertEqual(observer1.retrieved, [entities])
     }
     
-    func test_subscribeAfterSuccessfulLoad_notifiesWithPreviouslyLoadedCats() {
+    func test_subscribeAfterSuccessfulLoad_notifiesWithPreviouslyLoadedEntities() {
         let (sut, loader) = makeSut()
         
-        sut.subscribeCats()
-        let cats = makeCats()
-        loader.complete(with: cats)
+        sut.subscribeEntities()
+        let entities = makeEntities()
+        loader.complete(with: entities)
         
-        XCTAssertEqual(SpyObserver<[Cat]>(sut: sut).retrieved, [cats])
+        XCTAssertEqual(SpyObserver<[Entity]>(sut: sut).retrieved, [entities])
     }
     
-    func test_loadCompletionWithCats_doesNotNotifyError() {
+    func test_loadCompletionWithEntities_doesNotNotifyError() {
         let (sut, loader) = makeSut()
         
-        sut.subscribeCats()
+        sut.subscribeEntities()
         loader.complete(with: [])
         
         XCTAssertEqual(SpyObserver<NSError>(sut: sut).retrieved, [])
@@ -69,13 +70,13 @@ class LoadingStorageTests: XCTestCase {
         let error = anyError()
         let errorObserver = SpyObserver<NSError>(sut: sut)
         
-        sut.subscribeCats()
+        sut.subscribeEntities()
         loader.complete(with: error, at: 0)
         
         XCTAssertEqual(errorObserver.retrieved, [error])
         XCTAssertEqual(SpyObserver<NSError>(sut: sut).retrieved, [])
         
-        sut.subscribeCats()
+        sut.subscribeEntities()
         loader.complete(with: error, at: 1)
         
         XCTAssertEqual(errorObserver.retrieved, [error, error])
@@ -85,13 +86,13 @@ class LoadingStorageTests: XCTestCase {
     func test_cancelSubscription_doesNotNotify() {
         let (sut, loader) = makeSut()
         
-        var retrieved = [[Cat]]()
+        var retrieved = [[Entity]]()
         sut.subscribe(onNext: { retrieved.append($0) }).cancel()
         _ = sut.subscribe(onNext: { retrieved.append($0) })
         
         XCTAssertEqual(retrieved, [])
         
-        loader.complete(with: makeCats())
+        loader.complete(with: makeEntities())
         XCTAssertEqual(retrieved, [])
     }
     
@@ -99,7 +100,7 @@ class LoadingStorageTests: XCTestCase {
         let (sut, loader) = makeSut()
         
         var retrieved = [NSError]()
-        sut.subscribeCats()
+        sut.subscribeEntities()
         sut.subscribe(onError: { retrieved.append($0 as NSError) }).cancel()
         _ = sut.subscribe(onError: { retrieved.append($0 as NSError) })
         
@@ -113,9 +114,9 @@ class LoadingStorageTests: XCTestCase {
     
     private func makeSut(
         file: StaticString = #file,
-        line: UInt = #line) -> (sut: Service, loader: CatsLoaderSpy)
+        line: UInt = #line) -> (sut: Service, loader: EntitiesLoaderSpy)
     {
-        let loader = CatsLoaderSpy()
+        let loader = EntitiesLoaderSpy()
         let sut = Service(loader: loader)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
@@ -124,36 +125,30 @@ class LoadingStorageTests: XCTestCase {
         return (sut, loader)
     }
     
-    private func makeCats() -> [Cat] {
-        [makeUniqueCat(), makeUniqueCat(), makeUniqueCat()]
-    }
-    
-    private func makeUniqueCat() -> Cat {
-        Cat(id: UUID(),
-            name: "some name",
-            imageUrls: [])
+    private func makeEntities() -> [Entity] {
+        [UUID(), UUID(), UUID()]
     }
     
     private func anyError() -> NSError {
         NSError(domain: "TestDomain", code: 0, userInfo: nil)
     }
     
-    fileprivate final class CatsLoaderSpy: Loader {
-        private var completions: [(Result<[Cat], Error>) -> ()] = []
+    fileprivate final class EntitiesLoaderSpy: Loader {
+        private var completions: [(Result<[Entity], Error>) -> ()] = []
         
         var loadCallCount: Int { completions.count }
         
-        func load(_ completion: @escaping (Result<[Cat], Error>) -> ()) {
+        func load(_ completion: @escaping (Result<[Entity], Error>) -> ()) {
             completions.append(completion)
         }
         
         func complete(
-            with cats: [Cat],
+            with entities: [Entity],
             at index: Int = 0,
             file: StaticString = #file,
             line: UInt = #line)
         {
-            complete(with: .success(cats), at: index, file: file, line: line)
+            complete(with: .success(entities), at: index, file: file, line: line)
         }
         
         func complete(
@@ -166,7 +161,7 @@ class LoadingStorageTests: XCTestCase {
         }
         
         func complete(
-            with result: Result<[Cat], Error>,
+            with result: Result<[Entity], Error>,
             at index: Int = 0,
             file: StaticString = #file,
             line: UInt = #line)
@@ -185,13 +180,13 @@ class LoadingStorageTests: XCTestCase {
 }
 
 private extension Service {
-    func subscribeCats() {
+    func subscribeEntities() {
         _ = subscribe(onNext: { _ in })
     }
 }
 
 
-private extension SpyObserver where Value == [Cat] {
+private extension SpyObserver where Value == [Service.Entity] {
     convenience init(sut: Service) {
         self.init(sut.subscribe(onNext:))
     }
