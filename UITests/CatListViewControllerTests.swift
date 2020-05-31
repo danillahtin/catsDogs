e41cb5a338351c9508f1e3b5abd6f69b6e83c925
Danil Lakhtin <danil_lahtin@mail.ru>
2020-05-31 16:55:47 +0300

    [+] publisher notification renders cats

--- UITests/CatListViewControllerTests.swift
+++ UITests/CatListViewControllerTests.swift
@@ -7,15 +7,23 @@
 //
 
 import XCTest
+import Core
+
 
 protocol Publisher {
-    func subscribe()
+    func subscribe(_ observer: @escaping ([Cat]) -> ())
 }
 
 final class CatListViewController: UIViewController {
     let tableView = UITableView()
     
     private var publisher: Publisher!
+    
+    private var cats: [Cat] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     convenience init(publisher: Publisher) {
         self.init()
@@ -27,17 +35,21 @@ final class CatListViewController: UIViewController {
         super.viewDidLoad()
         
         tableView.dataSource = self
-        publisher.subscribe()
+        publisher.subscribe { [weak self] in self?.cats = $0 }
     }
 }
 
 extension CatListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        cats.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = UITableViewCell()
+
+        cell.textLabel?.text = cats[indexPath.row].name
+        
+        return cell
     }
 }
 
@@ -58,6 +70,26 @@ class CatListViewControllerTests: XCTestCase {
         XCTAssertEqual(publisher.subscribers.count, 1)
     }
     
+    func test_publisherNotification_rendersCats() {
+        let publisher = PublisherStub()
+        let sut = makeSut(publisher: publisher)
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.renderedViewsCount, 0)
+        
+        let buffy = makeCat(name: "Buffy")
+        let buckwheat = makeCat(name: "Buckwheat")
+        
+        publisher.notify(with: [buffy])
+        XCTAssertEqual(sut.renderedViewsCount, 1)
+        XCTAssertEqual(sut.view(at: 0)?.title, "Buffy")
+
+        publisher.notify(with: [buckwheat, buffy])
+        XCTAssertEqual(sut.renderedViewsCount, 2)
+        XCTAssertEqual(sut.view(at: 0)?.title, "Buckwheat")
+        XCTAssertEqual(sut.view(at: 1)?.title, "Buffy")
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
@@ -73,11 +105,19 @@ class CatListViewControllerTests: XCTestCase {
         return sut
     }
     
+    private func makeCat(name: String = "noname") -> Cat {
+        Cat(id: UUID(), name: name, imageUrls: [])
+    }
+    
     private final class PublisherStub: Publisher {
-        var subscribers: [Void] = []
+        var subscribers: [([Cat]) -> ()] = []
         
-        func subscribe() {
-            subscribers.append(())
+        func subscribe(_ observer: @escaping ([Cat]) -> ()) {
+            subscribers.append(observer)
+        }
+        
+        func notify(with cats: [Cat]) {
+            subscribers.forEach({ $0(cats) })
         }
     }
 }
@@ -85,6 +125,19 @@ class CatListViewControllerTests: XCTestCase {
 
 private extension CatListViewController {
     var renderedViewsCount: Int {
-        tableView.dataSource!.tableView(tableView, numberOfRowsInSection: 0)
+        tableView.numberOfRows(inSection: 0)
+    }
+    
+    func view(at index: Int) -> UITableViewCell? {
+        let indexPath = IndexPath(row: index, section: 0)
+        
+        return tableView.dataSource?.tableView(tableView, cellForRowAt: indexPath)
+    }
+}
+
+
+private extension UITableViewCell {
+    var title: String? {
+        textLabel?.text
     }
 }
