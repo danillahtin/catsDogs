060595a70f53fc5c890983839780af0745eb6946
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-07 11:38:51 +0300

    [+ renders state when state is updated before loadView]

--- UITests/ProfileViewControllerTests.swift
+++ UITests/ProfileViewControllerTests.swift
@@ -10,14 +10,38 @@ import XCTest
 import UIKit
 
 
+enum ProfileState {
+    case authorized(String)
+    case unauthorized
+}
+
 class ProfileViewController: UIViewController {
     let profileViewContainerView = UIView()
     let signInButtonContainerView = UIView()
+    let profileNameLabel = UILabel()
+    
+    private var state: ProfileState = .unauthorized
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profileViewContainerView.isHidden = true
+        render(state: state)
+    }
+    
+    func profileUpdated(state: ProfileState) {
+        self.state = state
+    }
+    
+    func render(state: ProfileState) {
+        switch state {
+        case .authorized(let user):
+            signInButtonContainerView.isHidden = true
+            profileViewContainerView.isHidden = false
+            profileNameLabel.text = user
+        case .unauthorized:
+            signInButtonContainerView.isHidden = false
+            profileViewContainerView.isHidden = true
+        }
     }
 }
 
@@ -36,6 +60,12 @@ class ProfileViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.isProfileViewHidden, true)
     }
     
+    func test_rendersState_whenStateUpdatedBeforeLoadView() {
+        assertStateIsRenderedOnLoadView(.unauthorized)
+        assertStateIsRenderedOnLoadView(.authorized("User"))
+        assertStateIsRenderedOnLoadView(.authorized("Another user"))
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
@@ -48,6 +78,36 @@ class ProfileViewControllerTests: XCTestCase {
         
         return sut
     }
+    
+    private func assertStateIsRenderedOnLoadView(
+        _ state: ProfileState,
+        file: StaticString = #file,
+        line: UInt = #line)
+    {
+        let sut = makeSut()
+        
+        sut.profileUpdated(state: state)
+        sut.loadViewIfNeeded()
+        
+        assert(sut: sut, renders: state, file: file, line: line)
+    }
+    
+    private func assert(
+        sut: ProfileViewController,
+        renders state: ProfileState,
+        file: StaticString = #file,
+        line: UInt = #line)
+    {
+        switch state {
+        case .authorized(let user):
+            XCTAssertEqual(sut.isSignInButtonHidden, true, file: file, line: line)
+            XCTAssertEqual(sut.isProfileViewHidden, false, file: file, line: line)
+            XCTAssertEqual(sut.renderedProfileName, user, file: file, line: line)
+        case .unauthorized:
+            XCTAssertEqual(sut.isSignInButtonHidden, false, file: file, line: line)
+            XCTAssertEqual(sut.isProfileViewHidden, true, file: file, line: line)
+        }
+    }
 }
 
 
@@ -58,5 +118,9 @@ private extension ProfileViewController {
     
     var isProfileViewHidden: Bool {
         profileViewContainerView.isHidden
+    }
+    
+    var renderedProfileName: String? {
+        profileNameLabel.text
     }
 }
