b2dbbf913c9192c171dead904f72e5b9cf413c10
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-08 22:46:50 +0300

    [+] Init does not start components

--- CatsDogsTests/CoditionalFlowCompositeTests.swift
+++ CatsDogsTests/CoditionalFlowCompositeTests.swift
@@ -13,19 +13,28 @@ import XCTest
 class ConditionalFlowComposite {}
 
 class CoditionalFlowCompositeTests: XCTestCase {
-    func test() {
-        let sut = makeSut()
+    func test_init_doesNotStartComponents() {
+        let primary = FlowSpy()
+        let secondary = FlowSpy()
+        _ = makeSut(primary: primary, secondary: secondary)
+        
+        XCTAssertEqual(primary.startedCount, 0)
+        XCTAssertEqual(secondary.startedCount, 0)
     }
     
     // MARK: - Helpers
     
     private func makeSut(
+        primary: FlowSpy = .init(),
+        secondary: FlowSpy = .init(),
         file: StaticString = #file,
         line: UInt = #line) -> ConditionalFlowComposite
     {
         let sut = ConditionalFlowComposite()
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: primary, file: file, line: line)
+        trackMemoryLeaks(for: secondary, file: file, line: line)
         
         return sut
     }
