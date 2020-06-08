006739ad44367249b4f94a724a88454baa87c098
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-08 22:50:17 +0300

    [+] start starts primary when condition is true

--- CatsDogsTests/CoditionalFlowCompositeTests.swift
+++ CatsDogsTests/CoditionalFlowCompositeTests.swift
@@ -10,7 +10,17 @@ import XCTest
 @testable import CatsDogs
 
 
-class ConditionalFlowComposite {}
+class ConditionalFlowComposite: Flow {
+    let primary: Flow
+    
+    init(primary: Flow) {
+        self.primary = primary
+    }
+    
+    func start() {
+        primary.start()
+    }
+}
 
 class CoditionalFlowCompositeTests: XCTestCase {
     func test_init_doesNotStartComponents() {
@@ -22,15 +32,32 @@ class CoditionalFlowCompositeTests: XCTestCase {
         XCTAssertEqual(secondary.startedCount, 0)
     }
     
+    func test_start_startsPrimaryWhenConditionIsTrue() {
+        let primary = FlowSpy()
+        let secondary = FlowSpy()
+        let sut = makeSut(primary: primary, secondary: secondary, condition: true)
+        
+        sut.start()
+        
+        XCTAssertEqual(primary.startedCount, 1)
+        XCTAssertEqual(secondary.startedCount, 0)
+        
+        sut.start()
+        
+        XCTAssertEqual(primary.startedCount, 2)
+        XCTAssertEqual(secondary.startedCount, 0)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
         primary: FlowSpy = .init(),
         secondary: FlowSpy = .init(),
+        condition: Bool = true,
         file: StaticString = #file,
-        line: UInt = #line) -> ConditionalFlowComposite
+        line: UInt = #line) -> Flow
     {
-        let sut = ConditionalFlowComposite()
+        let sut = ConditionalFlowComposite(primary: primary)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
         trackMemoryLeaks(for: primary, file: file, line: line)
