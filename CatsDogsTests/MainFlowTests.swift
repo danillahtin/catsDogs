abb1ef1d1520f39182112d6a74d548f5aaa93d5f
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-08 23:40:04 +0300

    [+] init does not set view controllers

--- CatsDogsTests/MainFlowTests.swift
+++ CatsDogsTests/MainFlowTests.swift
@@ -11,22 +11,44 @@ import XCTest
 
 final class MainFlow {}
 
+protocol UINavigationControllerProtocol {
+    func setViewControllers(_ viewControllers: [UIViewController], animated: Bool)
+}
+
 
 class MainFlowTests: XCTestCase {
-    func test() {
-        let sut = makeSut()
+    func test_init_doesNotSet() {
+        let (_, navigationController) = makeSut()
+        
+        XCTAssertEqual(navigationController.messages, [])
     }
     
     // MARK: - Helpers
     
     private func makeSut(
         file: StaticString = #file,
-        line: UInt = #line) -> MainFlow
+        line: UInt = #line) -> (sut: MainFlow, navigationController: NavigationControllerSpy)
     {
+        let navigationController = NavigationControllerSpy()
         let sut = MainFlow()
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: navigationController, file: file, line: line)
         
-        return sut
+        return (sut, navigationController)
+    }
+    
+    
+}
+
+final class NavigationControllerSpy: UINavigationControllerProtocol {
+    enum Message: Equatable {
+        case set(viewControllers: [UIViewController], animated: Bool)
+    }
+    
+    var messages: [Message] = []
+    
+    func setViewControllers(_ viewControllers: [UIViewController], animated: Bool) {
+        messages.append(.set(viewControllers: viewControllers, animated: animated))
     }
 }
