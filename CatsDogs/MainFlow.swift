a8ccb5eca2e5ac1f6119ecdf0e9cb739a39483e2
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 00:15:08 +0300

    [*] moves MainFlow to production

--- CatsDogs/MainFlow.swift
+++ CatsDogs/MainFlow.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2020 Danil Lahtin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+final class MainFlow: Flow {
+    typealias ViewControllerBuilder = () -> UIViewController
+    
+    private let navigationController: UINavigationControllerProtocol
+    private let catsViewControllerBuilder: ViewControllerBuilder
+    private let dogsViewControllerBuilder: ViewControllerBuilder
+    private let profileViewControllerBuilder: ViewControllerBuilder
+    
+    init(
+        catsViewControllerBuilder: @escaping ViewControllerBuilder,
+        dogsViewControllerBuilder: @escaping ViewControllerBuilder,
+        profileViewControllerBuilder: @escaping ViewControllerBuilder,
+        navigationController: UINavigationControllerProtocol)
+    {
+        self.navigationController = navigationController
+        self.catsViewControllerBuilder = catsViewControllerBuilder
+        self.dogsViewControllerBuilder = dogsViewControllerBuilder
+        self.profileViewControllerBuilder = profileViewControllerBuilder
+    }
+    
+    func start() {
+        let catsVc = catsViewControllerBuilder()
+        let dogsVc = dogsViewControllerBuilder()
+        let profileVc = profileViewControllerBuilder()
+        
+        catsVc.tabBarItem = UITabBarItem(title: "Cats", image: nil, selectedImage: nil)
+        dogsVc.tabBarItem = UITabBarItem(title: "Dogs", image: nil, selectedImage: nil)
+        profileVc.tabBarItem = UITabBarItem(title: "Profile", image: nil, selectedImage: nil)
+        
+        let vc = UITabBarController()
+        vc.viewControllers = [catsVc, dogsVc, profileVc]
+        
+        navigationController.setViewControllers([vc], animated: true)
+    }
+}
--- CatsDogsTests/MainFlowTests.swift
+++ CatsDogsTests/MainFlowTests.swift
@@ -10,43 +10,6 @@ import XCTest
 @testable import CatsDogs
 
 
-final class MainFlow: Flow {
-    typealias ViewControllerBuilder = () -> UIViewController
-    
-    private let navigationController: UINavigationControllerProtocol
-    private let catsViewControllerBuilder: ViewControllerBuilder
-    private let dogsViewControllerBuilder: ViewControllerBuilder
-    private let profileViewControllerBuilder: ViewControllerBuilder
-    
-    init(
-        catsViewControllerBuilder: @escaping ViewControllerBuilder,
-        dogsViewControllerBuilder: @escaping ViewControllerBuilder,
-        profileViewControllerBuilder: @escaping ViewControllerBuilder,
-        navigationController: UINavigationControllerProtocol)
-    {
-        self.navigationController = navigationController
-        self.catsViewControllerBuilder = catsViewControllerBuilder
-        self.dogsViewControllerBuilder = dogsViewControllerBuilder
-        self.profileViewControllerBuilder = profileViewControllerBuilder
-    }
-    
-    func start() {
-        let catsVc = catsViewControllerBuilder()
-        let dogsVc = dogsViewControllerBuilder()
-        let profileVc = profileViewControllerBuilder()
-        
-        catsVc.tabBarItem = UITabBarItem(title: "Cats", image: nil, selectedImage: nil)
-        dogsVc.tabBarItem = UITabBarItem(title: "Dogs", image: nil, selectedImage: nil)
-        profileVc.tabBarItem = UITabBarItem(title: "Profile", image: nil, selectedImage: nil)
-        
-        let vc = UITabBarController()
-        vc.viewControllers = [catsVc, dogsVc, profileVc]
-        
-        navigationController.setViewControllers([vc], animated: true)
-    }
-}
-
-
 class MainFlowTests: XCTestCase {
     func test_init_doesNotSet() {
         let (_, navigationController) = makeSut()
