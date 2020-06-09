e490d43f2438ce37363cc4874d80b009eadfd730
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 23:50:31 +0300

    [+] loginViewController didSkip completes

--- CatsDogsTests/PushAuthFlowTests.swift
+++ CatsDogsTests/PushAuthFlowTests.swift
@@ -13,13 +13,20 @@ import UI
 
 final class PushAuthFlow {
     let navigationController: UINavigationControllerProtocol
+    let onComplete: () -> ()
     
-    init(navigationController: UINavigationControllerProtocol) {
+    init(navigationController: UINavigationControllerProtocol,
+         onComplete: @escaping () -> ())
+    {
         self.navigationController = navigationController
+        self.onComplete = onComplete
     }
     
     func start() {
-        navigationController.setViewControllers([LoginViewController()], animated: true)
+        let vc = LoginViewController()
+        vc.didSkip = onComplete
+        
+        navigationController.setViewControllers([vc], animated: true)
     }
 }
 
@@ -45,18 +52,40 @@ class PushAuthFlowTests: XCTestCase {
         XCTAssertNotNil(loginViewController)
     }
     
+    func test_loginViewControllerDidSkip_completes() {
+        var completedCount = 0
+        let (sut, navigationController) = makeSut(onComplete: { completedCount += 1 })
+        
+        sut.start()
+        let rootVc = navigationController.messages[0].viewControllers?.first
+        let loginViewController = rootVc as? LoginViewController
+
+        XCTAssertEqual(completedCount, 0)
+        loginViewController?.didSkip()
+        
+        XCTAssertEqual(completedCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
+        onComplete: @escaping () -> () = {},
         file: StaticString = #file,
         line: UInt = #line) -> (sut: PushAuthFlow, navigationController: NavigationControllerSpy)
     {
         let navigationControllerSpy = NavigationControllerSpy()
-        let sut = PushAuthFlow(navigationController: navigationControllerSpy)
+        let sut = PushAuthFlow(
+            navigationController: navigationControllerSpy,
+            onComplete: onComplete)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
         trackMemoryLeaks(for: navigationControllerSpy, file: file, line: line)
         
         return (sut, navigationControllerSpy)
+    }
+    
+    private func getLoginViewController(from navigationController: NavigationControllerSpy) -> LoginViewController? {
+        let rootVc = navigationController.messages[0].viewControllers?.first
+        return rootVc as? LoginViewController
     }
 }
