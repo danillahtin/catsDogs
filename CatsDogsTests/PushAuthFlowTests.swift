5715965cf69ca7eabee73186fd857f265712b1fa
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 16:11:39 +0300

    [+] PushAuthFlow init does not set

--- CatsDogsTests/PushAuthFlowTests.swift
+++ CatsDogsTests/PushAuthFlowTests.swift
@@ -7,25 +7,30 @@
 //
 
 import XCTest
+@testable import CatsDogs
 
 
 final class PushAuthFlow {}
 
 class PushAuthFlowTests: XCTestCase {
-    func test() {
-        let sut = makeSut()
+    func test_init_doesNotSet() {
+        let (_, navigationController) = makeSut()
+
+        XCTAssertEqual(navigationController.messages, [])
     }
     
     // MARK: - Helpers
     
     private func makeSut(
         file: StaticString = #file,
-        line: UInt = #line) -> PushAuthFlow
+        line: UInt = #line) -> (sut: PushAuthFlow, navigationController: NavigationControllerSpy)
     {
+        let navigationControllerSpy = NavigationControllerSpy()
         let sut = PushAuthFlow()
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: navigationControllerSpy, file: file, line: line)
         
-        return sut
+        return (sut, navigationControllerSpy)
     }
 }
