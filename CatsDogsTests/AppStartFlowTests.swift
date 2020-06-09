495cb1185530c611466ba7b564fb65c2837a3bed
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 12:57:57 +0300

    [+] start starts session check

--- CatsDogsTests/AppStartFlowTests.swift
+++ CatsDogsTests/AppStartFlowTests.swift
@@ -8,24 +8,49 @@
 
 import XCTest
 
+final class SessionCheckingSpy {
+    private(set) var requestsCount = 0
+    
+    func check() {
+        requestsCount += 1
+    }
+}
 
-final class AppStartFlow {}
+final class AppStartFlow {
+    let sessionChecking: SessionCheckingSpy
+    
+    init(sessionChecking: SessionCheckingSpy) {
+        self.sessionChecking = sessionChecking
+    }
+    
+    func start() {
+        sessionChecking.check()
+    }
+}
 
 
 class AppStartFlowTests: XCTestCase {
-    func test() {
-        let sut = makeSut()
+    func test_start_startsSessionCheck() {
+        let sessionChecking = SessionCheckingSpy()
+        let sut = makeSut(sessionChecking: sessionChecking)
+        
+        XCTAssertEqual(sessionChecking.requestsCount, 0)
+        sut.start()
+        
+        XCTAssertEqual(sessionChecking.requestsCount, 1)
     }
     
     // MARK: - Helpers
     
     private func makeSut(
+        sessionChecking: SessionCheckingSpy = .init(),
         file: StaticString = #file,
         line: UInt = #line) -> AppStartFlow
     {
-        let sut = AppStartFlow()
+        let sut = AppStartFlow(sessionChecking: sessionChecking)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: sessionChecking, file: file, line: line)
         
         return sut
     }
