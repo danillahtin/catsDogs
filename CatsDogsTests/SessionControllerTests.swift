832022375ea5daaa8eb476fcdc0bc567dfa452a1
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 14:42:10 +0300

    [+] check starts tokenLoading

--- CatsDogsTests/SessionControllerTests.swift
+++ CatsDogsTests/SessionControllerTests.swift
@@ -9,22 +9,51 @@
 import XCTest
 
 
-final class SessionController {}
+final class TokenStoreSpy {
+    private(set) var loadCallCount = 0
+    
+    func load() {
+        loadCallCount += 1
+    }
+}
+
+final class SessionController {
+    let tokenStore: TokenStoreSpy
+    
+    init(tokenStore: TokenStoreSpy) {
+        self.tokenStore = tokenStore
+    }
+    
+    func check() {
+        tokenStore.load()
+    }
+}
 
 class SessionControllerTests: XCTestCase {
     func test() {
         let _ = makeSut()
     }
     
+    func test_check_startsTokenLoading() {
+        let tokenStore = TokenStoreSpy()
+        let sut = makeSut(tokenStore: tokenStore)
+        
+        XCTAssertEqual(tokenStore.loadCallCount, 0)
+        sut.check()
+        XCTAssertEqual(tokenStore.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
+        tokenStore: TokenStoreSpy = .init(),
         file: StaticString = #file,
         line: UInt = #line) -> SessionController
     {
-        let sut = SessionController()
+        let sut = SessionController(tokenStore: tokenStore)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: tokenStore, file: file, line: line)
         
         return sut
     }
