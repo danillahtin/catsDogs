c3b3bea4cbe0e6df0c7868d204828ad39e1190a1
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 13:02:58 +0300

    [+] start does not start flows

--- CatsDogsTests/AppStartFlowTests.swift
+++ CatsDogsTests/AppStartFlowTests.swift
@@ -32,7 +32,7 @@ final class AppStartFlow {
 class AppStartFlowTests: XCTestCase {
     func test_start_startsSessionCheck() {
         let sessionChecking = SessionCheckingSpy()
-        let sut = makeSut(sessionChecking: sessionChecking)
+        let (sut, _, _) = makeSut(sessionChecking: sessionChecking)
         
         XCTAssertEqual(sessionChecking.requestsCount, 0)
         sut.start()
@@ -40,18 +40,31 @@ class AppStartFlowTests: XCTestCase {
         XCTAssertEqual(sessionChecking.requestsCount, 1)
     }
     
+    func test_start_doesNotStartFlows() {
+        let (sut, main, auth) = makeSut()
+
+        sut.start()
+        
+        XCTAssertEqual(main.startedCount, 0)
+        XCTAssertEqual(auth.startedCount, 0)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
         sessionChecking: SessionCheckingSpy = .init(),
         file: StaticString = #file,
-        line: UInt = #line) -> AppStartFlow
+        line: UInt = #line) -> (sut: AppStartFlow, main: FlowSpy, auth: FlowSpy)
     {
+        let main = FlowSpy()
+        let auth = FlowSpy()
         let sut = AppStartFlow(sessionChecking: sessionChecking)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
+        trackMemoryLeaks(for: main, file: file, line: line)
+        trackMemoryLeaks(for: auth, file: file, line: line)
         trackMemoryLeaks(for: sessionChecking, file: file, line: line)
         
-        return sut
+        return (sut, main, auth)
     }
 }
