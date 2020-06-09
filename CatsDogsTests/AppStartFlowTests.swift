08453ef619593850e7487cc9da66945b5a600f6a
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-09 13:10:03 +0300

    [+] session check completion with .exists starts main

--- CatsDogsTests/AppStartFlowTests.swift
+++ CatsDogsTests/AppStartFlowTests.swift
@@ -7,24 +7,51 @@
 //
 
 import XCTest
+@testable import CatsDogs
+
+enum SessionCheckResult {
+    case exists
+}
 
 final class SessionCheckingSpy {
-    private(set) var requestsCount = 0
+    private var completions: [(SessionCheckResult) -> ()] = []
+    var requestsCount: Int { completions.count }
     
-    func check() {
-        requestsCount += 1
+    func check(_ completion: @escaping (SessionCheckResult) -> () = { _ in }) {
+        completions.append(completion)
+    }
+    
+    func complete(
+        with result: SessionCheckResult,
+        at index: Int = 0,
+        file: StaticString = #file,
+        line: UInt = #line)
+    {
+        guard completions.indices.contains(index) else {
+            XCTFail(
+                "Completion at index \(index) not found, has only \(completions.count) completions",
+                file: file,
+                line: line)
+            return
+        }
+        
+        completions[index](result)
     }
 }
 
 final class AppStartFlow {
     let sessionChecking: SessionCheckingSpy
+    let main: Flow
     
-    init(sessionChecking: SessionCheckingSpy) {
+    init(sessionChecking: SessionCheckingSpy, main: Flow) {
         self.sessionChecking = sessionChecking
+        self.main = main
     }
     
     func start() {
-        sessionChecking.check()
+        sessionChecking.check { [main] _ in
+            main.start()
+        }
     }
 }
 
@@ -49,6 +76,18 @@ class AppStartFlowTests: XCTestCase {
         XCTAssertEqual(auth.startedCount, 0)
     }
     
+    func test_sessionCheckCompletionWithExists_startsMain() {
+        let sessionChecking = SessionCheckingSpy()
+        let (sut, main, auth) = makeSut(sessionChecking: sessionChecking)
+        
+        sut.start()
+        
+        sessionChecking.complete(with: .exists)
+        
+        XCTAssertEqual(main.startedCount, 1)
+        XCTAssertEqual(auth.startedCount, 0)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
@@ -58,7 +97,7 @@ class AppStartFlowTests: XCTestCase {
     {
         let main = FlowSpy()
         let auth = FlowSpy()
-        let sut = AppStartFlow(sessionChecking: sessionChecking)
+        let sut = AppStartFlow(sessionChecking: sessionChecking, main: main)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
         trackMemoryLeaks(for: main, file: file, line: line)
