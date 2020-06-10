6d870b3ede66c0cf5cbbe2eb1a5c36aadc5eb5b6
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-10 05:34:22 +0300

    [+] adds dogs getter to remote api

--- CatsDogs/Api.swift
+++ CatsDogs/Api.swift
@@ -50,6 +50,15 @@ final class Api {
         Cat(id: UUID(), name: "Flash", imageUrl: URL(string: "https://i.pinimg.com/236x/7a/2c/ca/7a2cca034f2a4b1fbf7c99b17ac1a687.jpg")!),
     ]
     
+    private let dogs: [Dog] = [
+        Dog(id: UUID(), name: "Rufus", imageUrl: URL(string: "https://info-4all.ru/images/1ea26de91427124bfe753493a0963382.jpg")!),
+        Dog(id: UUID(), name: "Pumba", imageUrl: URL(string: "https://i.pinimg.com/236x/3a/5d/f8/3a5df859c29c0929252d6f3e007157b8.jpg")!),
+        Dog(id: UUID(), name: "Butters", imageUrl: URL(string: "https://www.hvostik.by/upload/information_system_5/2/2/8/item_228/information_items_228.jpg")!),
+        Dog(id: UUID(), name: "Snowball", imageUrl: URL(string: "https://avatars.mds.yandex.net/get-pdb/940654/1a2be3be-4024-4d86-8c3e-c3b560f94886/s375")!),
+        Dog(id: UUID(), name: "Bob", imageUrl: URL(string: "https://img.uslugio.com/img/2f/a0/2fa03b0ae56b42d1615b37169abd7c4e.jpg")!),
+        Dog(id: UUID(), name: "Peanut", imageUrl: URL(string: "https://avatars.mds.yandex.net/get-pdb/1927216/fc90329e-9835-4fba-af14-09269535ed57/s375")!),
+    ]
+    
     private func dispatch(_ block: @escaping () -> ()) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
             block()
@@ -115,6 +124,24 @@ extension Api {
             }
             
             completion(.success(self.cats))
+        }
+    }
+    
+    func dogs(_ completion: @escaping (Result<[Dog], Error>) -> ()) {
+        dispatch { [weak self] in
+            guard let self = self else {
+                return completion(.failure(Error.serverUnavailable))
+            }
+            
+            guard let token = self.token else {
+                return completion(.failure(Error.unauthorized))
+            }
+            
+            guard token.expirationDate > Date() else {
+                return completion(.failure(Error.unauthorized))
+            }
+            
+            completion(.success(self.dogs))
         }
     }
 }
