23e247a24dfdf62c80228605e56c3670a177875b
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-10 07:44:25 +0300

    [*] Moves UserDefaultsTokenStore to production

---
+++ CatsDogs/UserDefaultsTokenStore.swift
@@ -0,0 +1,63 @@
+//
+//  UserDefaultsTokenStore.swift
+//  CatsDogs
+//
+//  Created by Danil Lahtin on 10.06.2020.
+//  Copyright © 2020 Danil Lahtin. All rights reserved.
+//
+
+import Foundation
+import Core
+
+final class UserDefaultsTokenStore {
+    private struct CodableToken: Codable {
+        let login: String
+        let password: String
+        let expirationDate: Date
+        
+        init(token: AccessToken) {
+            self.login = token.credentials.login
+            self.password = token.credentials.password
+            self.expirationDate = token.expirationDate
+        }
+        
+        func token() -> AccessToken {
+            AccessToken(credentials: Credentials(login: login, password: password),
+                        expirationDate: expirationDate)
+        }
+    }
+    
+    enum Error: Swift.Error {
+        case dataNotFound
+    }
+    
+    let userDefaults: UserDefaults
+    private let key = "UserDefaultsTokenStore.tokenKey"
+    
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    func save(token: AccessToken, completion: @escaping (Result<Void, Swift.Error>) -> ()) {
+        do {
+            let data = try JSONEncoder().encode(CodableToken(token: token))
+            userDefaults.set(data, forKey: key)
+            completion(.success(()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func load(_ completion: @escaping (Result<AccessToken, Swift.Error>) -> ()) {
+        guard let data = userDefaults.data(forKey: key) else {
+            return completion(.failure(Error.dataNotFound))
+        }
+        
+        do {
+            let token = try JSONDecoder().decode(CodableToken.self, from: data)
+            completion(.success(token.token()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
--- CatsDogsTests/UserDefaultsTokenStoreTests.swift
+++ CatsDogsTests/UserDefaultsTokenStoreTests.swift
@@ -10,58 +10,6 @@ import XCTest
 import Core
 @testable import CatsDogs
 
-final class UserDefaultsTokenStore {
-    private struct CodableToken: Codable {
-        let login: String
-        let password: String
-        let expirationDate: Date
-        
-        init(token: AccessToken) {
-            self.login = token.credentials.login
-            self.password = token.credentials.password
-            self.expirationDate = token.expirationDate
-        }
-        
-        func token() -> AccessToken {
-            AccessToken(credentials: Credentials(login: login, password: password),
-                        expirationDate: expirationDate)
-        }
-    }
-    
-    enum Error: Swift.Error {
-        case dataNotFound
-    }
-    
-    let userDefaults: UserDefaults
-    private let key = "UserDefaultsTokenStore.tokenKey"
-    
-    init(userDefaults: UserDefaults) {
-        self.userDefaults = userDefaults
-    }
-    
-    func save(token: AccessToken, completion: @escaping (Result<Void, Swift.Error>) -> ()) {
-        do {
-            let data = try JSONEncoder().encode(CodableToken(token: token))
-            userDefaults.set(data, forKey: key)
-            completion(.success(()))
-        } catch {
-            completion(.failure(error))
-        }
-    }
-    
-    func load(_ completion: @escaping (Result<AccessToken, Swift.Error>) -> ()) {
-        guard let data = userDefaults.data(forKey: key) else {
-            return completion(.failure(Error.dataNotFound))
-        }
-        
-        do {
-            let token = try JSONDecoder().decode(CodableToken.self, from: data)
-            completion(.success(token.token()))
-        } catch {
-            completion(.failure(error))
-        }
-    }
-}
 
 class UserDefaultsTokenStoreTests: XCTestCase {
     private var userDefaults: UserDefaults!
