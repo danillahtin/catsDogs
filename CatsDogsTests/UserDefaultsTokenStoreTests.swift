bff1962d0925c4afffe1b6df5bf3c6d6fae5fc9f
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-10 07:43:19 +0300

    [*] Moves load and save to separate methods

--- CatsDogsTests/UserDefaultsTokenStoreTests.swift
+++ CatsDogsTests/UserDefaultsTokenStoreTests.swift
@@ -74,35 +74,20 @@ class UserDefaultsTokenStoreTests: XCTestCase {
     }
     
     func test_load_returnsErrorWhenNothingSaved() {
-        var retrieved: [Result<AccessToken, NSError>] = []
-        makeSut().load {
-            retrieved.append($0.mapError({ $0 as NSError }))
-        }
+        let loadResult = load()
         
-        XCTAssertEqual(retrieved.count, 1)
-        XCTAssertThrowsError(try retrieved.first?.get())
+        XCTAssertEqual(loadResult.count, 1)
+        XCTAssertThrowsError(try loadResult.first?.get())
     }
     
     func test_load_returnsSavedToken() {
         let token = makeToken()
         
-        let expSave = expectation(description: "Wait for save")
-        makeSut().save(token: token, completion: { _ in
-            expSave.fulfill()
-        })
+        let saveResult = save(token: token)
+        XCTAssertEqual(saveResult.count, 1)
+        XCTAssertNoThrow(try saveResult.first?.get())
         
-        wait(for: [expSave], timeout: 5.0)
-        
-        let expLoad = expectation(description: "Wait for load")
-        var retrieved: [Result<AccessToken, NSError>] = []
-        makeSut().load {
-            retrieved.append($0.mapError({ $0 as NSError }))
-            expLoad.fulfill()
-        }
-        
-        wait(for: [expLoad], timeout: 5.0)
-        
-        XCTAssertEqual(retrieved, [.success(token)])
+        XCTAssertEqual(load(), [.success(token)])
     }
     
     // MARK: - Helpers
@@ -122,5 +107,32 @@ class UserDefaultsTokenStoreTests: XCTestCase {
         AccessToken(
             credentials: Credentials(login: "login", password: "password"),
             expirationDate: Date(timeIntervalSince1970: 0))
+    }
+    
+    private func save(token: AccessToken) -> [Result<Void, NSError>] {
+        var retrieved: [Result<Void, NSError>] = []
+        
+        let expSave = expectation(description: "Wait for save")
+        makeSut().save(token: token, completion: {
+            retrieved.append($0.mapError({ $0 as NSError }))
+            expSave.fulfill()
+        })
+        
+        wait(for: [expSave], timeout: 5.0)
+        
+        return retrieved
+    }
+    
+    private func load() -> [Result<AccessToken, NSError>] {
+        let expLoad = expectation(description: "Wait for load")
+        var retrieved: [Result<AccessToken, NSError>] = []
+        makeSut().load {
+            retrieved.append($0.mapError({ $0 as NSError }))
+            expLoad.fulfill()
+        }
+        
+        wait(for: [expLoad], timeout: 5.0)
+        
+        return retrieved
     }
 }
