56b8f05c193c30f86acb9321da729a14f319012a
Danil Lakhtin <danil_lahtin@mail.ru>
2020-06-10 07:34:57 +0300

    [+] load returns saved token

--- CatsDogsTests/UserDefaultsTokenStoreTests.swift
+++ CatsDogsTests/UserDefaultsTokenStoreTests.swift
@@ -7,22 +7,102 @@
 //
 
 import XCTest
+import Core
 @testable import CatsDogs
 
-class UserDefaultsTokenStore {
-    func load(_ completion: @escaping (Result<AccessToken, Error>) -> ()) {
+final class UserDefaultsTokenStore {
+    private struct CodableToken: Codable {
+        let login: String
+        let password: String
+        let expirationDate: Date
         
+        init(token: AccessToken) {
+            self.login = token.credentials.login
+            self.password = token.credentials.password
+            self.expirationDate = token.expirationDate
+        }
+        
+        func token() -> AccessToken {
+            AccessToken(credentials: Credentials(login: login, password: password),
+                        expirationDate: expirationDate)
+        }
+    }
+    
+    enum Error: Swift.Error {
+        case dataNotFound
+    }
+    
+    let userDefaults: UserDefaults
+    private let key = "UserDefaultsTokenStore.tokenKey"
+    
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+    
+    func save(token: AccessToken, completion: @escaping (Result<Void, Swift.Error>) -> ()) {
+        do {
+            let data = try JSONEncoder().encode(CodableToken(token: token))
+            userDefaults.set(data, forKey: key)
+            completion(.success(()))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func load(_ completion: @escaping (Result<AccessToken, Swift.Error>) -> ()) {
+        guard let data = userDefaults.data(forKey: key) else {
+            return completion(.failure(Error.dataNotFound))
+        }
+        
+        do {
+            let token = try JSONDecoder().decode(CodableToken.self, from: data)
+            completion(.success(token.token()))
+        } catch {
+            completion(.failure(error))
+        }
     }
 }
 
 class UserDefaultsTokenStoreTests: XCTestCase {
-    func test_load_returnsNotFoundErrorWhenNothingSaved() {
+    private var userDefaults: UserDefaults!
+    
+    override func setUp() {
+        super.setUp()
+        
+        userDefaults = UserDefaults(suiteName: #file)
+        userDefaults.removePersistentDomain(forName: #file)
+    }
+    
+    func test_load_returnsErrorWhenNothingSaved() {
         var retrieved: [Result<AccessToken, NSError>] = []
         makeSut().load {
             retrieved.append($0.mapError({ $0 as NSError }))
         }
         
-        XCTAssertEqual(retrieved, [])
+        XCTAssertEqual(retrieved.count, 1)
+        XCTAssertThrowsError(try retrieved.first?.get())
+    }
+    
+    func test_load_returnsSavedToken() {
+        let token = makeToken()
+        
+        let expSave = expectation(description: "Wait for save")
+        makeSut().save(token: token, completion: { _ in
+            expSave.fulfill()
+        })
+        
+        wait(for: [expSave], timeout: 5.0)
+        
+        let expLoad = expectation(description: "Wait for load")
+        var retrieved: [Result<AccessToken, NSError>] = []
+        makeSut().load {
+            retrieved.append($0.mapError({ $0 as NSError }))
+            expLoad.fulfill()
+        }
+        
+        wait(for: [expLoad], timeout: 5.0)
+        
+        XCTAssertEqual(retrieved, [.success(token)])
     }
     
     // MARK: - Helpers
@@ -31,10 +111,16 @@ class UserDefaultsTokenStoreTests: XCTestCase {
         file: StaticString = #file,
         line: UInt = #line) -> UserDefaultsTokenStore
     {
-        let sut = UserDefaultsTokenStore()
+        let sut = UserDefaultsTokenStore(userDefaults: userDefaults)
         
         trackMemoryLeaks(for: sut, file: file, line: line)
         
         return sut
+    }
+    
+    private func makeToken() -> AccessToken {
+        AccessToken(
+            credentials: Credentials(login: "login", password: "password"),
+            expirationDate: Date(timeIntervalSince1970: 0))
     }
 }
